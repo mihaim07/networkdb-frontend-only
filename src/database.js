aea060c896642ac1e3ab9bea84bc7f74aa54967b
mihaim07 <florynmogo@gmail.com>
2022-07-14 21:40:14 +0300

    adding basic db file

---
+++ src/database.js
@@ -0,0 +1,92 @@
+export const database = [
+    {
+      id: 1,
+      name: 'ABC',
+      location: 'Pitesti',
+      vid: 100,
+      ipAdd: '192.168.100.1',
+      mask: '255.255.255.252',
+      info: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'ABC',
+      location: 'Costesti',
+      vid: 200,
+      ipAdd: '192.168.200.1',
+      mask: '255.255.255.252',
+      info: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'ABC',
+      location: 'Topoloveni',
+      vid: 150
+      ipAdd: '192.168.150.1',
+      mask: '255.255.255.252',
+      info: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'FGH',
+      location: 'Pitesti',
+      vid: 102,
+      ipAdd: '172.16.102.1',
+      mask: '255.255.255.240',
+      info: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'MNP',
+      location: 'Pitesti',
+      vid: 101,
+      ipAdd: '10.10.101.1',
+      mask: '255.255.255.252',
+      info: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'MNP',
+      location: 'Costesti',
+      vid: 201,
+      ipAdd: '10.10.201.1',
+      mask: '255.255.255.252',
+      info: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'XYZ',
+      location: 'Pitesti',
+      vid: 103,
+      ipAdd: '192.168.103.1',
+      mask: '255.255.255.252',
+      info: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'XYZ',
+      location: 'Costesti',
+      vid: 200,
+      ipAdd: '192.168.200.1',
+      mask: '255.255.255.252',
+      info: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'XYZ',
+      location: 'Topoloveni',
+      vid: 151,
+      ipAdd: '192.168.151.1',
+      mask: '255.255.255.252',
+      info: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Mioveni',
+      location: 'Moriah.Stanton',
+      vid: 250,
+      ipAdd: '192.168.250.1',
+      mask: '255.255.255.252',
+      info: 'Rey.Padberg@karina.biz'
+    }
+  ];
